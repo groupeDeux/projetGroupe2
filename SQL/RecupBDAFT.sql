2744ed00536a55bb6e114226fc129eaa09730b6f
fureta <agathefuret@gmail.com>
2015-03-04 14:26:32 +0100

    Recup BD
    nouveau doc RecupBrutAFT.sql
    modif doc RecupBDAFT

--- SQL/RecupBDAFT.sql
+++ SQL/RecupBDAFT.sql
@@ -1,2 +1,11 @@
 Insert into Epreuve
-    select nepreuve, nome, discipline, categorie, (dateepr+
+    select nepreuve, nome, discipline, categorie,(dateepr+(8/24)), (dateepr+(17/24)), 'NULL',50,100
+    from lesepreuves;
+
+insert into EpreuveIndividuelle
+    select numEpreuve
+
+insert into delegation
+    select distinct pays
+    from lessportifs
+    
