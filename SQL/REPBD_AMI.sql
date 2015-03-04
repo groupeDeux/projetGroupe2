547fdf626f44c45e3624bc06e5b78c5fc6ca8a07
Arbia Magouri <biamagouri@hotmail.fr>
2015-03-04 16:10:40 +0100

    reprise AMI (reverted from commit fd6af73916fb38f8380023ec051dbd6af0f3e738)

--- SQL/REPBD_AMI.sql
+++ SQL/REPBD_AMI.sql
@@ -1,39 +1,13 @@
-/*LesSportifs*/
-INSERT INTO  LesSportifs(numSportif,nom, prenom,dateNaissance,genre) 
+INSERT INTO  LesSportifs 
 SELECT NS, NOM, PRENOM, DATENAIS, CATEGORIE
-FROM INILESSPORTIFS;
+FROM JO.LESSPORTIFS;
 
-/*LesEquipes*/
-INSERT INTO LesEquipes(numEquipe,categorie)  
+INSERT INTO LesEquipes  
 SELECT NEQUIPE,CATEGORIE
-FROM INILESSPORTIFSEQ;
-
-/*LesBatiments*/
-SELECT INTO LesBatiments (nomBatiment, numeroRue,rue,ville)
-select NOMBAT,NUMERO,RUE,VILLE
-FROM INILESLOGEMENTSINIT;
-
-/* LesChambres*/
-SELECT INTO LesChambres (numChambre, capacite)
-select NLOGEMENT,CAPACITE
-FROM INILESLOGEMENTSINIT;
-
-/* LesDelegations*/
-SELECT INTO LesDelegations (pays)
-select distinct PAYS
-FROM INILESSPORTIFS;
+FROM JO.LESSPORTIFSEQ;
 
 
-/*LesDelegations(NBSPORTIF)*/
-SELECT INTO ViewLesDelegations (nbSportif,pays)
-select COUNT (DISTINCT NS) AS NBSPORTIF,PAYS
-FROM JO.LESSPORTIFS
-GROUP BY PAYS ;
 
-/*LesParticipants*/
-INSERT INTO lesParticipants(idParticipant, idDelegation)
-select NS, idDelegation
-from INILESSPORTIFS
-JOIN LesDelegations
-USING (pays); 
+
+
 
