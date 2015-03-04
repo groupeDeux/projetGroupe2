07034a8b70a1e6efbd19f51a22995bbbc2456f94
fureta <agathefuret@gmail.com>
2015-03-04 16:03:14 +0100

    recup BD
    mise a jour

--- SQL/RecupBDAFT.sql
+++ SQL/RecupBDAFT.sql
@@ -1,11 +1,16 @@
-Insert into Epreuve
+/* Insert into Epreuve
     select nepreuve, nome, discipline, categorie,(dateepr+(8/24)), (dateepr+(17/24)), 'NULL',50,100
     from lesepreuves;
 
 insert into EpreuveIndividuelle
-    select numEpreuve
+    select numEpreuve */
 
-insert into delegation
-    select distinct pays
-    from lessportifs
+
+/* Les participants*/
+Insert into LesParticipants(idDelegation)
+    select idDelegation
+    from IniSportifs S
+        join LesDelegations D
+        on (S.idDelegation=D.idDelegation);
+)
     
