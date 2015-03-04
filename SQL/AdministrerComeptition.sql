cb0def181924dc52221f8306550c8da5e3ff7ef1
Andreea Oprisor <Andreea.Oprisor1@e.ujf-grenoble.fr>
2015-03-04 10:32:21 +0100

    Create Tables in AdministrerCompetition

--- SQL/AdministrerComeptition.sql
+++ SQL/AdministrerComeptition.sql
@@ -1,20 +1,25 @@
-CREATE TABLE LesDiciplines (
-nomDisciplne VARCHAR(30),
-Constraint LDi_PK PRIMARY KEY nomDiscipline
-);
+DROP TABLE LesDisciplines;
+CREATE TABLE LesDisciplines
+(
+nomDiscipline varchar(30),
+Constraint LDi_PK PRIMARY KEY (nomDiscipline)
+); 
 
-CREATE TABLE LesEpreuves (
+CREATE TABLE LesEpreuves(
 idEpreuve INTEGER,
-nomEpreuve VARCHAR(30),
-nomDiscipline VARCHAR(30),
-categorie VARCHAR(30),
+nomEpreuve VARCHAR(30) NOT NULL,
+nomDiscipline VARCHAR(30) NOT NULL,
+categorie VARCHAR(30) NOT NULL,
 dateDebut date,
 dateFin date,
 urlVideo VARCHAR(100),
 tarif NUMBER(5,2),
 nombreDePlace INTEGER,
-Constraint LEp_PK PRIMARY KEY idEpreuve,
-Constraint LEp_FK1 FOREIGN KEY nomDiscipline REFERENCES LesDisciplines(nomDiscipline),
-Constraint LEp_C1 unique urlVideo,
-Constraint LEp_C3 CHECK dateDebut < DateFin)
+Constraint LEp_PK PRIMARY KEY (idEpreuve),
+Constraint LEp_FK1 FOREIGN KEY (nomDiscipline) REFERENCES LesDisciplines(nomDiscipline),
+Constraint LEp_C1 unique (urlVideo),
+Constraint LEp_C3 CHECK (dateDebut < DateFin)
 );
+
+
+
